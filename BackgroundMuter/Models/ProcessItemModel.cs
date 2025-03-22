f6b0cbbf7a5f537120e183258dce7e9d5cbd6996
Mark Lierman <marklierman@hotmail.com>
2025-03-22 13:29:02 -0400

    Add INotify to ProcessItemModel

--- BackgroundMuter/Models/ProcessItemModel.cs
+++ BackgroundMuter/Models/ProcessItemModel.cs
@@ -4,16 +4,69 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace BackgroundMuter.Models
 {
-    public class ProcessItemModel
+    public class ProcessItemModel : INotifyPropertyChanged
     {
-        public string ProcessName { get; set; }
-        public string? ProcessWindowTitle { get; set; }
-        public string DisplayName { get; set; }
-        public nint ProcessHandle { get; set; }
-        public Process Process { get; set; }
-        public bool IsBeingWatched { get; set; }
+        private string processName;
+        private string? processWindowTitle;
+        private string displayName;
+        private nint processHandle;
+        private Process process;
+        private bool isBeingWatched;
+
+        public string ProcessName 
+        { 
+            get => processName;
+            set => SetProperty(ref processName, value);
+        }
+
+        public string? ProcessWindowTitle 
+        { 
+            get => processWindowTitle;
+            set => SetProperty(ref processWindowTitle, value);
+        }
+
+        public string DisplayName 
+        { 
+            get => displayName;
+            set => SetProperty(ref displayName, value);
+        }
+
+        public nint ProcessHandle 
+        { 
+            get => processHandle;
+            set => SetProperty(ref processHandle, value);
+        }
+
+        public Process Process 
+        { 
+            get => process;
+            set => SetProperty(ref process, value);
+        }
+
+        public bool IsBeingWatched 
+        { 
+            get => isBeingWatched;
+            set => SetProperty(ref isBeingWatched, value);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
